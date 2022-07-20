1a446f7d33cd188b886bc6846301dc3b4b0279ac
Akingbade675 <akingbadebasit26@gmail.com>
2022-07-20 08:46:18 -0700

    fixed bugs in 0x08 Recursion project

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,13 +1,24 @@
 #include "main.h"
 
+
+/**
+ * _puts - prints a string, followed by a new line
+ * @s: string to be printed
+ */
+void _puts(char *s)
+{
+	if (s[0] == '\0')
+		return;
+	_putchar(s[0]);
+	_puts(s + 1);
+}
+
 /**
  * _puts_recursion - prints a string, followed by a new line
  * @s: string to be printed
  */
 void _puts_recursion(char *s)
 {
-	if (s[0] == '\0')
-		return;
-	_putchar(s[0]);
-	_puts_recursion(s + 1);
+	_puts(s);
+	_putchar('\n');
 }
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,19 @@
-int _isprime(unsigned int, int);
+/**
+ * _isprime - determine if n is a prime number
+ * @n: number
+ * @i: iterator
+ *
+ * Return: 1 is n is prime and 0 if not
+ */
+
+int _isprime(int n, int i)
+{
+	if (n == i || i > 9)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	return (_isprime(n, i + 1));
+}
 
 /**
  * is_prime_number - determine if prime number
@@ -12,22 +27,4 @@ int is_prime_number(int n)
 	if (n < 2)
 		return (0);
 	return (_isprime(n, 2));
-}
-
-
-/**
- * _isprime - determine if n is a prime number
- * @n: number
- * @i: iterator
- *
- * Return: 1 is n is prime and 0 if not
- */
-
-int _isprime(unsigned int n, int i)
-{
-	if (n == i || i > 9)
-		return (1);
-	if (n % i == 0)
-		return (0);
-	return (_isprime(n, i + 1));
 }
