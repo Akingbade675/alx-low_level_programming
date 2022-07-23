181133bb7877feb9586c5b55e0d551a22c1f023c
Akingbade675 <akingbadebasit26@gmail.com>
2022-07-23 15:01:35 -0700

    solved the bug in file 100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,39 +2,55 @@
 #include <stdlib.h>
 
 /**
- * main - coins to make change for an amount of money
- * @argc: argument counter
- * @argv: array of pointer to strings
+ * change - determine the minimum number of coins to make change \
+ * for an amount of money
+ * @argv: argument vector
  *
- * Return: 0 if no errors, else
+ * Return: the minimum number of coins to make change for an amount of money
  */
-int main(int argc, char *argv[])
+int change(int argv)
 {
-	int a, n = 0, i, t;
-	int c[5] = {25, 10, 5, 2, 1};
+	static int i, c;
+	int v[5] = {25, 10, 5, 2, 1};
 
-	if (argc != 2)
+	if (argv == 0)
 	{
-		puts("Error");
-		return (1);
-	}
-	a = atoi(argv[1]);
-	if (a <= 0)
-	{
-		puts("0");
-		return (1);
+		return (c);
 	}
 	else
 	{
-		for (i = 0; i < 5; i++)
+		while (argv >= v[i])
 		{
-			t = a / c[i];
-			a -= t * c[i];
-			n += t;
-			if (a == 0)
-			break;
+			argv -= v[i];
+			c++;
 		}
+		i++;
+		return (change(argv));
 	}
-	printf("%d\n", n);
+}
+
+
+/**
+ * main - prints the minimum number of coins to make change for an amount \
+ * of money
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 1 if argc == 1, otherwise 0 always
+ */
+int main(int argc, char *argv[])
+{
+	int c;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	c = atoi(argv[1]);
+	if (c < 0)
+		printf("0\n");
+	else
+		printf("%d\n", change(c));
 	return (0);
 }
