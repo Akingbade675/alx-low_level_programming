d59a569b6c5a5c732c22c1e3e5d8a866fc205e6f
Akingbade675 <akingbadebasit26@gmail.com>
2022-07-26 04:24:09 -0700

    trying to fix bug

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,21 +9,20 @@ array of integers to 0
  */
 int **alloc_grid(int width, int height)
 {
-	int **arr, *ar, h = 0, i, j;
+	int **arr = NULL, h = 0, j;
 
 	/* check If width or height is 0 or negative */
 	if (width < 1 || height < 1)
 		return (NULL);
 	while (h < height)
 	{
-		ar = malloc(sizeof(int) * width);
-		if (ar == NULL)
+		arr[h] = malloc(sizeof(int) * width);
+		if (arr[h] == NULL)
+			free(arr[h]);
 			return (NULL);
-		arr = &ar;
 		h++;
-	}
-	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
-			arr[i][j] = 0;
+			arr[h][j] = 0;
+	}
 	return (arr);
 }
