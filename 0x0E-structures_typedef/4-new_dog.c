361f098a90e84c269a6b9e40ca48035f3d6f9d92
Akingbade675 <akingbadebasit26@gmail.com>
2022-08-01 14:47:22 -0700

    fixing bugs in so

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,18 +16,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 
 	dog = malloc(sizeof(dog_t));
+	if (!dog)
+		return (NULL);
 
 	/* length of string name + null terminator*/
 	for (sn = 1; name[sn]; sn++)
 		;
-
 	dog->name = malloc(sizeof(char) * sn);
 	if (!dog->name)
 	{
 		free(dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < sn; i++)
 		dog->name[i] = name[i];
 
@@ -36,7 +36,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 	/* length of string owner + null terminator*/
 	for (sn = 1; owner[sn]; sn++)
 		;
-
 	dog->owner = malloc(sizeof(char) * sn);
 	if (!dog->owner)
 	{
@@ -44,7 +43,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-
 	for (i = 0; i < sn; i++)
 		dog->owner[i] = owner[i];
 
