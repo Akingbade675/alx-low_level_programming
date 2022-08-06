e0d1e8c0d3cac81877bef3f6e0a3f75b7adca5f8
Akingbade675 <akingbadebasit26@gmail.com>
2022-08-05 22:31:21 -0700

    added solution file for task 3 of project

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,19 +8,12 @@
  */
 int main(int argc, char **argv)
 {
-	int result;
 	int (*op_func)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
-	}
-
-	if ((*(argv[2]) == '/' || *(argv[2]) == '%') && atoi(argv[3]) == 0)
-	{
-		printf("Error\n");
-		exit(100);
 	}
 
 	op_func = get_op_func(argv[2]);
@@ -30,8 +23,7 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 
-	result = op_func(atoi(argv[1]), atoi(argv[3]));
-	printf("%d\n", result);
+	printf("%d\n", op_func(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,3 +1,6 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
 /**
  * op_add - sum a and b
  * @a: first operand
@@ -42,6 +45,11 @@ int op_mul(int a, int b)
  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
@@ -54,6 +62,11 @@ int op_div(int a, int b)
  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
 
