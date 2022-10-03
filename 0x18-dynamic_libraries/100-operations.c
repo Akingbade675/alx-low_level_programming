ead688057be73ab126be7dc4344ab84741fa061f
Akingbade675 <akingbadebasit26@gmail.com>
2022-10-03 22:58:34 +0000

    added dynamic libraries advanced tasks

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+/**
+ * add - adds two number
+ * @a: number a
+ * @b: number b
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two number
+ * @a: number a
+ * @b: number b
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two number
+ * @a: number a
+ * @b: number b
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two number
+ * @a: number a
+ * @b: number b
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - adds two number
+ * @a: number a
+ * @b: number b
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
